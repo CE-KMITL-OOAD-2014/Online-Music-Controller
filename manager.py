cc05d0abe13de69733eb648fe95b09b925cb409d
SAYONG <sls.slp36@gmail.com>
2014-10-23 01:28:25 +0700

    modify manager

--- manager.py
+++ manager.py
@@ -6,9 +6,13 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
+import torndb
 from tornado.options import define, options
 define("port", default=8080, help="run on the given port", type=int)
-
+define("mysql_host", default="127.0.0.1:8080", help="blog database host")
+define("mysql_database", default="myDB", help="blog database name")
+define("mysql_user", default="root", help="blog database user")
+define("mysql_password", default="root", help="blog database password")
  
 clients = []
 
@@ -53,12 +57,31 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
 		self.com = Command.PlaySong()
 		self.result =  self.man.user_player.run_command(self.com,"lalala")
 		self.write(self.result)
-        
+
+class RegisterHandler(tornado.web.RequestHandler):
+	self.db = torndb.Connection(
+		host=options.mysql_host, database=options.mysql_database,
+		user=options.mysql_user, password=options.mysql_password)
+
+	def get(self):
+		self.render("regis.html")
+
+	def post(self):
+		self.name = self.get_argument("name")
+		self.mail = self.get_argument("email")
+		self.password = self.get_argument("pass")
+		self.db.execute(
+			"INSERT INTO user (user,email,password) VALUES (self.name,self.mail,self.password)" 
+			) 	
+		self.redirect("/")
+     		
+     		     
 
 
 application = tornado.web.Application([
     (r"/", IndexHandler),
     (r"/ws", WebSocketHandler),
+    (r"/regis", RegisterHandler),
     (r"/return", TestHandler)],
     template_path=os.path.join(os.path.dirname(__file__), "templates"),
 	static_path=os.path.join(os.path.dirname(__file__), "static"),
