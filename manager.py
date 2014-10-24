0af65951a65b5cce1ad0e5dc0a44e8707bb37210
SAYONG <sls.slp36@gmail.com>
2014-10-25 05:27:28 +0700

    coomit chang auth system

--- manager.py
+++ manager.py
@@ -1,43 +1,78 @@
 import os.path
 from module import User,Player,Command
 
+import os.path
+import torndb
+import tornado.auth
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import unicodedata
 import tornado.websocket
-import torndb
+
 from tornado.options import define, options
-define("port", default=8080, help="run on the given port", type=int)
+define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
 define("mysql_database", default="myDB", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="root", help="blog database password")
- 
+
 clients = []
 
-class IndexHandler(tornado.web.RequestHandler):
+class Application(tornado.web.Application):
+    def __init__(self): 
+        handlers = [
+            (r"/", IndexHandler),
+            (r"/ws", WebSocketHandler),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler),
+                
+        ]
+        settings = dict(
+            blog_title=u"Tornado Blog",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            cookie_secret="bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+            login_url="/auth/login",
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        # Have one global connection to the blog DB across all handlers
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password) 
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("cookie_user")
+        if not user_id: return None
+        return self.db.get("SELECT * FROM user WHERE id = %s", int(user_id))
+
+
+class IndexHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         self.render("index.html")
 
-class TestHandler(tornado.web.RequestHandler): 
-	def post(self):
-		var = self.get_argument('var')
-		self.man = User.User(var)
-		self.com = Command.PlaySong()
-		self.resualt =  self.man.user_player.run_command(self.com,"lalala")
-		self.write(self.resualt)
 
 class PlayerManagment():
-	def __init__(self,command) :
-		if command == "play_pause" :
-			self.com = Command.PlayPause()
-		elif command == "next":
-			self.com = Command.Next() 
-		elif command == "previous":
-			self.com = Command.Previous()
-		self.user = User.User("Sukrit")
-		print self.user.user_player.run_command(self.com)
+    def __init__(self,command) :
+        if command == "play_pause" :
+            self.com = Command.PlayPause()
+        elif command == "next":
+            self.com = Command.Next() 
+        elif command == "previous":
+            self.com = Command.Previous()
+        self.user = User.User("Sukrit")
+        print self.user.user_player.run_command(self.com)
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
@@ -52,41 +87,56 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
         PlayerManagment(message)
 
     def post(self):
-		var = self.get_argument('var')
-		self.man = User.User(var)
-		self.com = Command.PlaySong()
-		self.result =  self.man.user_player.run_command(self.com,"lalala")
-		self.write(self.result)
+        var = self.get_argument('var')
+        self.man = User.User(var)
+        self.com = Command.PlaySong()
+        self.result =  self.man.user_player.run_command(self.com,"lalala")
+        self.write(self.result)
 
-class RegisterHandler(tornado.web.RequestHandler):
-	
-	def get(self):
-		self.render("regis.html")
+class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        if self.get_argument("openid.mode", None):
+            self.get_authenticated_user(self._on_auth)
+            return
+        self.authenticate_redirect()
 
-	def post(self):
-		self.db = torndb.Connection(
-			options.mysql_host, options.mysql_database,
-			options.mysql_user, options.mysql_password)
+    def _on_auth(self, user):
+        if not user:
+            raise tornado.web.HTTPError(500, "Google auth failed")
+        author = self.db.get("SELECT * FROM user WHERE email = %s",
+                             user["email"])
+        if not author:
+            # Auto-create first author
+            any_author = self.db.get("SELECT * FROM user LIMIT 1")
+            if not any_author:
+                author_id = self.db.execute(
+                    "INSERT INTO user (email,name) VALUES (%s,%s)",
+                    user["email"], user["name"])
+            else:
+                self.redirect("/")
+                return
+        else:
+            author_id = author["id"]
+        self.set_secure_cookie("cookie_user", str(author_id))
+        self.redirect(self.get_argument("next", "/"))
 
-		self.name = self.get_argument("name")
-		self.mail = self.get_argument("email")
-		self.password = self.get_argument("pass")
-		self.db.execute(
-			"INSERT INTO user (user,email,password) VALUES (%s,%s,%s)",self.name,self.mail,self.password) 
-		self.redirect("/")
-     		
-     		     
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("cookie_user")
+        self.redirect(self.get_argument("next", "/"))
 
 
-application = tornado.web.Application([
-    (r"/", IndexHandler),
-    (r"/ws", WebSocketHandler),
-    (r"/regis", RegisterHandler),
-    (r"/return", TestHandler)],
-    template_path=os.path.join(os.path.dirname(__file__), "templates"),
-	static_path=os.path.join(os.path.dirname(__file__), "static"),
-	debug=True)
+
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
-    application.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
