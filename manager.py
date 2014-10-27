a22d33c33d416d36d6ba944dc95aa1ab429f4682
SAYONG <sls.slp36@gmail.com>
2014-10-27 20:42:08 +0700

    coomit revert

--- manager.py
+++ manager.py
@@ -1,25 +1,16 @@
 import os.path
 from module import User,Player,Command
 
-import re
-import os.path
 import torndb
-import tornado.auth
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import unicodedata
 import tornado.websocket
 
 from tornado.options import define, options
-<<<<<<< HEAD
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-=======
-define("port", default=8080, help="run on the given port", type=int)
-define("mysql_host", default="127.0.0.1:8080", help="blog database host")
->>>>>>> parent of 7ca7ff7... commit manager change
 define("mysql_database", default="myDB", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="root", help="blog database password")
@@ -31,9 +22,7 @@ class Application(tornado.web.Application):
         handlers = [
             (r"/", IndexHandler),
             (r"/ws", WebSocketHandler),
-            (r"/auth/login", AuthLoginHandler),
-            (r"/auth/logout", AuthLogoutHandler),
-                
+    		(r"/regis", RegisterHandler),               
         ]
         settings = dict(
             blog_title=u"Tornado Blog",
@@ -41,7 +30,7 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             xsrf_cookies=True,
             cookie_secret="bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
-            login_url="/auth/login",
+            login_url="/login",
             debug=True,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
@@ -64,7 +53,7 @@ class BaseHandler(tornado.web.RequestHandler):
 
 
 class IndexHandler(BaseHandler):
-    @tornado.web.authenticated
+    #@tornado.web.authenticated
     def get(self):
         self.render("index.html")
 
@@ -93,50 +82,11 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
         PlayerManagment(message)
 
     def post(self):
-<<<<<<< HEAD
         var = self.get_argument('var')
         self.man = User.User(var)
         self.com = Command.PlaySong()
         self.result =  self.man.user_player.run_command(self.com,"lalala")
         self.write(self.result)
-    
-
-class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
-    @tornado.web.asynchronous
-    def get(self):
-        if self.get_argument("openid.mode", None):
-            self.get_authenticated_user(self._on_auth)
-            return
-        self.authenticate_redirect()
-
-    def _on_auth(self, user):
-        if not user:
-            raise tornado.web.HTTPError(500, "Google auth failed")
-        author = self.db.get("SELECT * FROM user WHERE email = %s",
-                             user["email"])
-        if not author:
-            # Auto-create first author
-            any_author = self.db.get("SELECT * FROM user LIMIT 1")
-            if not any_author:
-                author_id = self.db.execute(
-                    "INSERT INTO user (email,name) VALUES (%s,%s)",
-                    user["email"], user["name"])
-            else:
-                self.redirect("/")
-                return
-        else:
-            author_id = author["id"]
-        self.set_secure_cookie("cookie_user", str(author_id))
-        self.redirect(self.get_argument("next", "/"))
-
-
-class AuthLogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("cookie_user")
-        self.redirect(self.get_argument("next", "/"))
-
-
-
 
 
 def main():
@@ -145,17 +95,9 @@ def main():
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
-=======
-		var = self.get_argument('var')
-		self.man = User.User(var)
-		self.com = Command.PlaySong()
-		self.result =  self.man.user_player.run_command(self.com,"lalala")
-		self.write(self.result)
 
-class RegisterHandler(tornado.web.RequestHandler):
-	self.db = torndb.Connection(
-		host=options.mysql_host, database=options.mysql_database,
-		user=options.mysql_user, password=options.mysql_password)
+
+class RegisterHandler(BaseHandler):
 
 	def get(self):
 		self.render("regis.html")
@@ -171,16 +113,6 @@ class RegisterHandler(tornado.web.RequestHandler):
      		
      		     
 
-
-application = tornado.web.Application([
-    (r"/", IndexHandler),
-    (r"/ws", WebSocketHandler),
-    (r"/regis", RegisterHandler),
-    (r"/return", TestHandler)],
-    template_path=os.path.join(os.path.dirname(__file__), "templates"),
-	static_path=os.path.join(os.path.dirname(__file__), "static"),
-	debug=True)
->>>>>>> parent of 7ca7ff7... commit manager change
 
 if __name__ == "__main__":
     main()
