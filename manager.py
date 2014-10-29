05ff4fd21ebb0f130349f348064c91244f5661d2
knotgundam <matthanaphaisarn@gmail.com>
2014-10-29 14:51:26 +0700

    upload file success

--- manager.py
+++ manager.py
@@ -1,4 +1,4 @@
-import os.path
+
 from module import User,Player,Command
 
 import torndb
@@ -7,7 +7,8 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
-
+import os
+import subprocess
 from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
@@ -16,13 +17,15 @@ define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="root", help="blog database password")
 
 clients = []
+__UPLOADS__ = "/home/westlife/Desktop/uploadTest/upload/"
 
 class Application(tornado.web.Application):
     def __init__(self): 
         handlers = [
             (r"/", IndexHandler),
             (r"/ws", WebSocketHandler),
-    		(r"/regis", RegisterHandler),               
+            (r"/regis", RegisterHandler),
+            (r"/upload", FileManagment)               
         ]
         settings = dict(
             blog_title=u"Tornado Blog",
@@ -70,6 +73,29 @@ class PlayerManagment():
         print self.user.user_player.run_command(self.com)
 
 
+class FileManagment(tornado.web.RequestHandler):
+    def post(self): #upload from host to server
+        cwd = subprocess.Popen('pwd', stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
+        location = cwd.communicate()[0]
+        newline_location = location.find("\n")
+        location = location[:newline_location]+"/upload/"
+
+        player_address = "161.246.6.118"
+
+        fileinfo = self.request.files['filearg'][0]
+        print "fileinfo is", fileinfo
+        fname = fileinfo['filename']
+        try:
+            fh = open(location + fname, 'w')
+            fh.write(fileinfo['body'])
+            self.finish(fname + " is uploaded!! Check "+location+" folder")
+            os.system("sshpass -p raspberry scp "+location+fname+" pi@"+player_address+":/home/pi/code") #from server to player
+            print "sshpass -p raspberry scp /home/westlife/Desktop/scpTest/"+fname+" pi@"+player_address+":/home/pi/code"
+
+        except :
+            self.finish("duplicate_file!!!")
+
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
     def open(self):
         clients.append(self)
@@ -89,30 +115,30 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler): # Data Managment
         self.write(self.result)
 
 
+
+class RegisterHandler(BaseHandler):
+
+    def get(self):
+        self.render("regis.html")
+
+    def post(self):
+        self.name = self.get_argument("name")
+        self.mail = self.get_argument("email")
+        self.password = self.get_argument("pass")
+        self.db.execute(
+            "INSERT INTO user (user,email,password) VALUES (self.name,self.mail,self.password)" 
+            )   
+        self.redirect("/")
+
+
+        
+
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
-
-
-
-class RegisterHandler(BaseHandler):
-
-	def get(self):
-		self.render("regis.html")
-
-	def post(self):
-		self.name = self.get_argument("name")
-		self.mail = self.get_argument("email")
-		self.password = self.get_argument("pass")
-		self.db.execute(
-			"INSERT INTO user (user,email,password) VALUES (self.name,self.mail,self.password)" 
-			) 	
-		self.redirect("/")
-     		
-     		     
-
-
+            
+                 
 if __name__ == "__main__":
     main()
